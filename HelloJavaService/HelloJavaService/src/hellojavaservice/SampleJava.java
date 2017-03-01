2b34d07a96f5b99ea05ec7dfb8ee8ec2d4def198
vinoddev <vinod.devinbox@gmail.com>
2017-03-01 17:49:02 +0530

    hellojava service with sonar-cobertura

---
+++ HelloJavaService/HelloJavaService/src/hellojavaservice/SampleJava.java
@@ -0,0 +1,10 @@
+package hellojavaservice;
+
+public class SampleJava {
+    public SampleJava() {
+        super();
+    }
+    public String SampleInput(String name){
+        return "Hello "+name;
+    }
+}
